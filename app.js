d99f1c62894caad254a94525acba3ad32e9d58f2
arun <arunramachandran15@gmail.com>
2018-02-23 05:03:15 +0530

    Message changed

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 require('./server/routes')(app);
 app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the beginning of nothingness.',
+  message: 'Welcome to Global Friends Network',
 }));
 
 module.exports = app;
