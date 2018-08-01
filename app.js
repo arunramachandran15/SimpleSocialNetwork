3337c146712c09aa172b9073dffe5af08d69c527
Arun Ramachandran <arun.ramachandran@softcrylic.co.in>
2018-08-01 23:56:43 +0530

    cors issue fixed wildcard added

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 
 const app = express();
+var cors = require('cors')
+app.use(cors())
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
